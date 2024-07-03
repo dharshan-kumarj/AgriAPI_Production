09993cf73ff9f74d92c8460f9922ac5f33093010
dharshan-kumarj <jdharshankumar18@gmail.com>
2024-07-03 06:13:26 +0000

    updated CORS policy
    it is working properly for both WebAPP and App

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,10 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://farmapi.selfmade.one'], // Replace with your frontend URL
+    'allowed_origins' => ['https://farmapi.selfmade.one'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
